6a79f06edaa026539e7e1470b9cf1c1b6e2a3858
vinaygautam1505-source <vinaygautam1505@gmail.com>
2026-02-13 12:41:31 +0530

    Modify SQL query for incremental materialization

--- models/temp_sample.sql
+++ models/temp_sample.sql
@@ -1,2 +1,10 @@
-SELECT * 
-FROM {{source( ' flipkart_order_details', 'FLIPKARD') }}
+{{ config(
+    materialized='incremental',
+    unique_key='product_id'
+) }}
+
+SELECT *
+FROM {{ source('flipkart_order_details', 'FLIPKARD') }}
+{% if is_incremental() %}
+WHERE listing_at > (SELECT MAX(listing_at) FROM {{ this }})
+{% endif %}
